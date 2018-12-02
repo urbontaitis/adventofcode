5b4a2ddf574f471ea5fac710c758b53f2797bc40
Mindaugas Urbontaitis <mindaugas@urbontaitis.lt>
2018-12-02 16:45:56 +0100

    After day2 moving everything to remote git repo

---
+++ README.md
@@ -0,0 +1,4 @@
+# adventofcode-2018
+
+In my daily work, I'm using Java and decided to try something new.
+I have decided to challenge myself and try to complete the Advent Of Code in JavaScript.
---
+++ day1.spec.js
@@ -0,0 +1,62 @@
+import { loadInput, frequencyResult, firstFrequency } from './day1';
+
+describe('Day 1', () => {
+  describe('Load puzzle data', () => {
+    test('load puzzle input as array', () => {
+      expect(loadInput()).toBeInstanceOf(Array);
+    });
+
+    test('load all array items', () => {
+      expect(loadInput().length).toBe(991);
+    });
+    // FIXME calculates incorrect value
+    test.skip('frequency result of real data', () => {
+      const frequencies = loadInput();
+      expect(firstFrequency(frequencies)).toBe(256);
+    });
+
+    test('frequency result of real data', () => {
+      const frequencies = loadInput();
+      expect(frequencyResult(frequencies)).toBe(433);
+    });
+  });
+
+  describe('Sum frequencies', () => {
+    test('frequency result of positive numbers', () => {
+      const frequencies = [1, 1, 1];
+      expect(frequencyResult(frequencies)).toBe(3);
+    });
+
+    test('frequency result of negative numbers', () => {
+      const frequencies = [-1, -2, -3];
+      expect(frequencyResult(frequencies)).toBe(-6);
+    });
+
+    test('frequency result of postive and negative numbers', () => {
+      const frequencies = [1, 1, -2];
+      expect(frequencyResult(frequencies)).toBe(0);
+    });
+  })
+
+  describe('First frequency it reaches twice', () => {
+    test('+1, -1 first reaches 0 twice.', () => {
+      const frequencies = [1, -1];
+      expect(firstFrequency(frequencies)).toBe(0);
+    });
+
+    test('+3, +3, +4, -2, -4 first reaches 10 twice', () => {
+      const frequencies = [3, 3, 4, -2, -4];
+      expect(firstFrequency(frequencies)).toBe(10);
+    });
+
+    test('-6, +3, +8, +5, -6 first reaches 5 twice.', () => {
+      const frequencies = [-6, 3, 8, 5, -6];
+      expect(firstFrequency(frequencies)).toBe(5);
+    });
+
+    test('+7, +7, -2, -7, -4 first reaches 14 twice.', () => {
+      const frequencies = [7, 7, -2, -7, -4];
+      expect(firstFrequency(frequencies)).toBe(14);
+    });
+  });
+});
