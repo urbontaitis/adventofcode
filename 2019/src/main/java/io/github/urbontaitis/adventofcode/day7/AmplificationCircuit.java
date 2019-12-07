9a1aa339455dab4a007550e47beb8a547a9d84b9
Mindaugas Urbontaitis <mindaugas@urbontaitis.lt>
2019-12-07 18:50:16 +0200

    Day 7 - dirty solution

--- 2019/src/main/java/io/github/urbontaitis/adventofcode/day7/AmplificationCircuit.java
+++ 2019/src/main/java/io/github/urbontaitis/adventofcode/day7/AmplificationCircuit.java
@@ -2,26 +2,75 @@ package io.github.urbontaitis.adventofcode.day7;
 
 import io.github.urbontaitis.adventofcode.day5.IntcodeUpgrade;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class AmplificationCircuit {
 
-    private final List<Integer> state;
+  private final List<Integer> state;
 
-    public AmplificationCircuit(List<Integer> state) {
-        this.state = state;
+  public AmplificationCircuit(List<Integer> state) {
+    this.state = state;
+  }
+
+  public Integer calculateThruster(List<Integer> sequences) {
+    int thruster = 0;
+    for (Integer phase : sequences) {
+      thruster = (new IntcodeUpgrade(state)).executeAmplifier(thruster, phase);
     }
 
-    public Integer calculateThruster(List<Integer> sequences) {
-        int thruster = 0;
-        for (Integer phase : sequences) {
-            thruster = (new IntcodeUpgrade(state)).executeAmplifier(thruster, phase);
+    return thruster;
+  }
+
+  public Integer findMaxThruster() {
+    int maxThruster = 0;
+    List<List<Integer>> allSequences = generateSequences();
+
+    for(List<Integer> sequences : allSequences) {
+      int thruster = calculateThruster(sequences);
+      if (thruster > maxThruster) {
+        maxThruster = thruster;
+      }
+    }
+
+    return maxThruster;
+  }
+
+  private List<List<Integer>> generateSequences() {
+    String[] base = "01234".split("");
+    List<String> strings = new ArrayList<>();
+    permute(base, base.length, strings);
+
+    List<List<Integer>> result = new ArrayList<>();
+
+    for (String s : strings) {
+      List<Integer> integers =
+          Arrays.asList(s.split(",")).stream().map(Integer::valueOf).collect(Collectors.toList());
+      result.add(integers);
+    }
+
+    return result;
+  }
+
+  private static void swap(String[] base, int right, int left) {
+    String temp = base[right];
+    base[right] = base[left];
+    base[left] = temp;
+  }
+
+  public static void permute(String[] base, int currentPosition, List<String> permutations) {
+    if (currentPosition == 1) {
+      permutations.add(String.join(",", base));
+    } else {
+      for (int i = 0; i < currentPosition; i++) {
+        permute(base, currentPosition - 1, permutations);
+
+        if (currentPosition % 2 == 1) {
+          swap(base, 0, currentPosition - 1);
+        } else {
+          swap(base, i, currentPosition - 1);
         }
-
-        return thruster;
+      }
     }
-
-    public Integer findMaxThruster() {
-        return 0;
-    }
+  }
 }
--- 2019/src/test/groovy/io/github/urbontaitis/adventofcode/day7/AmplificationCircuitSpec.groovy
+++ 2019/src/test/groovy/io/github/urbontaitis/adventofcode/day7/AmplificationCircuitSpec.groovy
@@ -1,7 +1,11 @@
 package io.github.urbontaitis.adventofcode.day7
 
+import io.github.urbontaitis.adventofcode.FileReader
+import io.github.urbontaitis.adventofcode.day5.IntcodeUpgrade
 import spock.lang.Specification
 import spock.lang.Unroll
+
+import java.util.stream.Collectors
 
 class AmplificationCircuitSpec extends Specification {
 
@@ -23,7 +27,6 @@ class AmplificationCircuitSpec extends Specification {
         65210       | [1, 0, 4, 3, 2] | [3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33, 1002, 33, 7, 33, 1, 33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0]
     }
 
-
     @Unroll
     def "Find highest signal - expected is #maxThruster"() {
         given:
@@ -41,6 +44,21 @@ class AmplificationCircuitSpec extends Specification {
         54321       | [3, 23, 3, 24, 1002, 24, 10, 24, 1002, 23, -1, 23, 101, 5, 23, 23, 1, 24, 23, 23, 4, 23, 99, 0, 0]
         65210       | [3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33, 1002, 33, 7, 33, 1, 33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0]
 
+    }
+
+
+    def "Find highest signal"() {
+        given:
+        String dataInputPath = "day7/input.txt"
+        String file = FileReader.readFile(dataInputPath)
+        List<Integer> dataInput = Arrays.stream(file.split(",")).map(Integer.&valueOf).collect(Collectors.toList())
+        AmplificationCircuit ac = new AmplificationCircuit(dataInput);
+
+        when:
+        Integer thruster = ac.findMaxThruster();
+
+        then:
+        thruster == 368584
     }
 
 }
