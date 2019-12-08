163944c968f661724f00ecdd6c9bdfb81db7bb91
Mindaugas Urbontaitis <mindaugas@urbontaitis.lt>
2019-12-08 13:11:29 +0200

    Day 6 - initial data

---
+++ 2019/src/test/groovy/io/github/urbontaitis/adventofcode/day6/UniversalOrbitMapSpec.groovy
@@ -0,0 +1,24 @@
+package io.github.urbontaitis.adventofcode.day6
+
+import spock.lang.Specification
+
+class UniversalOrbitMapSpec extends Specification {
+
+    UniversalOrbitMap testObject
+
+    def setup() {
+        testObject = new UniversalOrbitMap();
+    }
+
+    def "build tree from the list #list"() {
+        when:
+        def orbitMap = testObject.build(list)
+
+        then:
+        orbitMap == []
+
+        where:
+        list                                                                            | _
+        ['COM)B', 'B)C', 'C)D', 'D)E', 'E)F', 'B)G', 'G)H', 'D)I', 'E)J', 'J)K', 'K)L'] | _
+    }
+}
